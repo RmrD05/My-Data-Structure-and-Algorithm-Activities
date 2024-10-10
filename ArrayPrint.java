5ddc03e250ecf84a387ebc0cd9abae312d9d7219
RmrD05 <romardiamante@gmail.com>
2024-10-10 23:27:10 +0800

    Added the array imp and print

--- ArrayPrint.java
+++ ArrayPrint.java
@@ -1,9 +1,9 @@
-public class Array{
+public class ArrayPrint{
     public static void main(String [] args){
     String [] cars = {"Suzuki","Mazda","Honda", "Mercedes"};
   for(int i = 0; i< cars.length;i++){
       System.out.println(cars[i]);
       }
-  
+
     }
   }
